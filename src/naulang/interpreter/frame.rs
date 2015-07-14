75ef4cf73f54b45dcde21bbd14089a6944251afd
SamGiles <samuel.giles@ft.com>
2015-07-14 21:34:13 +0100

    Begin adding tests for simple things such as 'get_bytecode' at current program counter for current method

--- src/naulang/interpreter/frame.rs
+++ src/naulang/interpreter/frame.rs
@@ -4,16 +4,19 @@ use std::vec;
 pub struct Frame {
 	stack: vec::Vec<Object>,
 	stack_pointer: u32,
-	previous_frame: &Frame,
-	access_link: &Frame,
+	previous_frame: Option<Box<Frame>>,
+	access_link: Option<Box<Frame>>,
 	pc: u32,
 }
 
 impl Frame {
-	pub fn new(stack_size: u32) -> Frame {
-		Frame {
+	pub fn new(stack_size: u32) -> Box<Frame> {
+		Box::new(Frame {
 			stack: vec::Vec::new(),
 			stack_pointer: 0,
-		}
+			pc: 0,
+			access_link: Option::None,
+			previous_frame: Option::None
+		})
 	}
 }
--- src/naulang/objectspace/primitives.rs
+++ src/naulang/objectspace/primitives.rs
@@ -1,6 +1,8 @@
 use std::vec;
 use naulang::interpreter::frame::Frame;
 
+use naulang::interpreter::bytecode::ByteCode;
+
 pub enum Object {
 	String(StringObject),
 	Integer(IntegerObject),
@@ -13,10 +15,35 @@ pub enum Object {
 pub struct MethodObject {
 	literals: vec::Vec<Object>,
 	bytecodes: vec::Vec<u32>,
+	enclosing_frame: Option<Frame>,
 	arg_count: u32,
-	enclosing_frame: Option<Frame>
-	stack_depth: u32,
+	stack_depth: usize,
 	// TODO: SourceMaps
+}
+
+impl MethodObject {
+	pub fn new(bytecodes: vec::Vec<u32>) -> MethodObject {
+		MethodObject {
+			bytecodes: bytecodes.clone(),
+			literals: vec::Vec::new(),
+			enclosing_frame: Option::None,
+			arg_count: 0,
+			stack_depth: bytecodes.len(),
+		}
+	}
+
+	pub fn get_bytecode(&self, at_point: usize) -> u32 {
+		self.bytecodes[at_point] as u32
+	}
+}
+
+#[test]
+fn test_MethodObject_get_bytecode() {
+	let method: MethodObject = MethodObject::new(vec![
+		ByteCode.LOAD_CONST, 0,
+	]);
+
+	assert!(method.get_bytecode(0) == (ByteCode.LOAD_CONST as u32));
 }
 
 pub struct ArrayObject {
